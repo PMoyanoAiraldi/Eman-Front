1ec9677b3f53c498c4ffbf78d88c671ede781966
PMoyanoAiraldi <pmoyano497@gmail.com>
2026-05-22 15:06:57 -0300

    Acomodando estilos de Featured y creacion de ProductCard

--- eman-front/src/components/Featured/Featured.jsx
+++ eman-front/src/components/Featured/Featured.jsx
@@ -1,22 +1,19 @@
-import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import axios from 'axios'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import styles from './Featured.module.css'
+import { fetchFeaturedProducts } from '../../redux/productsReducer'
+import ProductCard from '../ProductCard/ProductCard'
 
 const FeaturedProducts = () => {
-    const [products, setProducts] = useState([])
-    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const { featuredProducts, loading } = useSelector(state => state.products)
     
 
     useEffect(() => {
-        axios.get('http://localhost:3010/products/featured')
-            .then(res => {
-                if (res.data?.length > 0) setProducts(res.data)
-            })
-            .catch(err => console.error('Error al cargar destacados:', err))
-    }, [])
+        dispatch(fetchFeaturedProducts())
+    }, [dispatch])
 
-    if (products.length === 0) return null
+    if (loading || featuredProducts.length === 0) return null
 
     return (
         <section className={styles.section}>
@@ -26,49 +23,9 @@ const FeaturedProducts = () => {
             </div>
 
             <div className={styles.grid}>
-                {products.map(product => {
-                    const image = product.images?.[0]?.url
-                    const hasStock = product.productSizes?.some(s => s.stock > 0)
-                    const sizesWithStock = product.productSizes?.filter(ps => ps.stock > 0)
-                    
-                    return (
-                        <article
-                            key={product.id}
-                            className={styles.card}
-                            onClick={() => navigate(`/producto/${product.id}`)}
-                        >
-                            <div className={styles.imageWrapper}>
-                                {image
-                                    ? <img src={image} alt={product.name} />
-                                    : <div className={styles.imagePlaceholder} />
-                                }
-                                {!hasStock && (
-                                    <span className={styles.soldOut}>Sin stock</span>
-                                )}
-                            </div>
-
-                            <div className={styles.info}>
-                                <span className={styles.category}>
-                                    {product.category?.name}
-                                </span>
-                                <h3 className={styles.name}>{product.name}</h3>
-                                <p className={styles.price}>
-                                    ${Number(product.price).toLocaleString('es-AR')}
-                                </p>
-                                {sizesWithStock?.length > 0 && (
-                            <div className={styles.sizes}>
-                                {sizesWithStock.map(ps => (
-                                    <span key={ps.id} className={styles.sizeTag}>
-                                        {ps.size.name}
-                                    </span>
-                                ))}
-                                </div>
-                            )}
-
-                            </div>
-                        </article>
-                    )
-                })}
+                {featuredProducts.map(product => (
+                    <ProductCard key={product.id} product={product} />
+                ))}
             </div>
         </section>
     )
--- eman-front/src/components/ProductCard/ProductCard.jsx
+++ eman-front/src/components/ProductCard/ProductCard.jsx
@@ -1,6 +1,43 @@
-const ProductCard = () => {
-    <>
+import { useNavigate } from 'react-router-dom'
+import styles from './ProductCard.module.css'
 
-    </>
+const ProductCard = ({ product }) => {
+    const navigate = useNavigate()
+    const image = product.images?.[0]?.url
+    const hasStock = product.productSizes?.some(s => s.stock > 0)
+    const sizesWithStock = product.productSizes?.filter(ps => ps.stock > 0)
+
+    return (
+        <article
+        className={styles.card}
+        onClick={() => navigate(`/producto/${product.id}`)}
+        >
+        <div className={styles.imageWrapper}>
+            {image
+            ? <img src={image} alt={product.name} />
+            : <div className={styles.imagePlaceholder} />
+            }
+            {!hasStock && <span className={styles.soldOut}>Sin stock</span>}
+        </div>
+
+        <div className={styles.info}>
+            <span className={styles.category}>{product.category?.name}</span>
+            <h3 className={styles.name}>{product.name}</h3>
+            <p className={styles.price}>
+            ${Number(product.price).toLocaleString('es-AR')}
+            </p>
+            {sizesWithStock?.length > 0 && (
+            <div className={styles.sizes}>
+                {sizesWithStock.map(ps => (
+                <span key={ps.id} className={styles.sizeTag}>
+                    {ps.size.name}
+                </span>
+                ))}
+            </div>
+            )}
+        </div>
+        </article>
+    )
 }
+
 export default ProductCard
